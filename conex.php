87d9fc7078de14dd1cf5f6e4d6e4d867fece551f
LalitoOwO <91231878+LalitoOwO@users.noreply.github.com>
2021-10-25 18:56:23 -0500

    Update conex.php

--- conex.php
+++ conex.php
@@ -1,1 +1,15 @@
+<?php
 
+$user = "root";
+$pass = "";
+$host = "localhost";
+$bd = "proyectofinal";
+
+
+$conexion = new mysqli($host, $user, $pass, $bd);
+
+if($conexion -> connect_errno){
+  echo "Error D. Conexion".$conexion->connect_error ."<br />";
+}
+
+?>
